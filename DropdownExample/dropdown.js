96f8157e0bc9cb2cfbe156fffd147297e11b8d88
Rameshvana <vanaparthiramesh741@gmail.com>
2022-07-31 23:22:45 +0530

    some test

---
+++ DropdownExample/dropdown.js
@@ -0,0 +1,16 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+  };
+  
+  let petSelect = document.getElementById('petSelect');
+  
+  petSelect.addEventListener('change',function(){
+    let selectedIndex=event.target.selectedIndex;
+    let dropdownValue=event.target.options[selectedIndex].text;
+      console.log(dropdownValue);
+  })
+  
